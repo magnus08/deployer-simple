e8badb87a3ef1bd5e55f08ddb4ff4e22b30cb591
Magnus Andersson <magnus.andersson@learnifier.com>
2017-07-04 14:47:05 +0200

    Simple endpoints for project

--- server.py
+++ server.py
@@ -5,12 +5,24 @@ import time
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        time.sleep(2)
-        return {'hello': 'world'}
 
-api.add_resource(HelloWorld, '/')
+projects = []
+projects.append({ 'id': 1, 'name': 'leweb', 'path': '', 'autoDeploy': False, 'redeploying': False, 'rebuilding': False })
+projects.append({ 'id': 2, 'name': 'boweb', 'path': '', 'autoDeploy': False, 'redeploying': False, 'rebuilding': False })
+projects.append({ 'id': 3, 'name': 'cpweb', 'path': '', 'autoDeploy': False, 'redeploying': False, 'rebuilding': False })
+
+class Projects(Resource):
+    def get(self):
+        return projects
+
+class Project(Resource):
+    def get(self, project_id):
+        time.sleep(2)
+        return {'hello': project_id}
+
+
+api.add_resource(Projects, '/projects')
+api.add_resource(Project, '/project/<int:project_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
